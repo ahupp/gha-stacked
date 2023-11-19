26d947c2bff68af4dbfbcbb192b45bc92673b19c
Adam Hupp <142939708+adamh-oai@users.noreply.github.com>
2023-11-18 19:59:29 -0800

    wip

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
 import os
-import requests  # noqa We are just importing this to prove the dependency installed correctly
+import requests 
 
 def main():
-    print(os.environ)
-    print(os.listdir("."))
+    owner_and_repo = os.environ["GITHUB_ACTION_REPOSITORY"]
+    event_name = os.environ["GITHUB_EVENT_NAME"]
+    auth_token = os.environ["INPUT_GITHUB-TOKEN"]
 
+    base_ref = os.environ["GITHUB_BASE_REF"]
+
+    if event_name != "pull_request":
+        print(f"Action not run on a pull_request, was {event_name}", file=sys.stderr)
+
+    print(locals())
+"""
+    r = requests.get(
+        f"https://api.github.com/repos/{owner_and_repo}/pulls/{pull_number}/commits",""
+        headers = {
+            "Authorization": f"Bearer {auth_token}",
+            "Accept": "application/vnd.github+json",
+            "X-GitHub-Api-Version", "2022-11-28",
+        }
+    )
+    r.raise_for_status()
+    pr_state = r.json()
+    print(pr_state)
+"""
+    print(os.environ)
+    
 if __name__ == "__main__":
     main()
