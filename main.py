9e6980455b352a7e49fdd369f7f1a65e80c14152
Adam Hupp <142939708+adamh-oai@users.noreply.github.com>
2023-11-18 20:05:30 -0800

    wip

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import os
 import requests 
 
 def main():
-    owner_and_repo = os.environ["GITHUB_ACTION_REPOSITORY"]
-    #event_name = os.environ["GITHUB_EVENT_NAME"]
+    #owner_and_repo = os.environ["GITHUB_ACTION_REPOSITORY"]
+    event_name = os.environ["GITHUB_EVENT_NAME"]
 
     base_ref = os.environ["GITHUB_BASE_REF"]
 
