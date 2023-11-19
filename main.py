5fe09f114457f037332bfeb084fc0d7bb6c5db2e
Adam Hupp <142939708+adamh-oai@users.noreply.github.com>
2023-11-18 22:14:34 -0800

    wip

--- main.py
+++ main.py
@@ -3,29 +3,19 @@ import requests
 
 def main():
     #owner_and_repo = os.environ["GITHUB_ACTION_REPOSITORY"]
-    event_name = os.environ["GITHUB_EVENT_NAME"]
+    #event_name = os.environ["GITHUB_EVENT_NAME"]
 
     base_ref = os.environ["GITHUB_BASE_REF"]
 
-    if event_name != "pull_request":
-        print(f"Action not run on a pull_request, was {event_name}", file=sys.stderr)
+    branch_prefixes = os.environ["INPUT_STACK-BRANCH-PREFIXES"]
+    branch_prefixes = branch_prefixes.split(",")
 
     print("base ref:", base_ref)
-    print(os.environ)
+    print("stack branch prefixes:", branch_prefixes)
+    
+    if any(base_ref.startswith(p) for p in branch_prefixes):
+        sys.exit(f"Base branch {base_ref} is an unmedged stacked PR, matches prefix in {branch_prefixes}")
 
-"""
-    r = requests.get(
-        f"https://api.github.com/repos/{owner_and_repo}/pulls/{pull_number}/commits",""
-        headers = {
-            "Authorization": f"Bearer {auth_token}",
-            "Accept": "application/vnd.github+json",
-            "X-GitHub-Api-Version", "2022-11-28",
-        }
-    )
-    r.raise_for_status()
-    pr_state = r.json()
-    print(pr_state)
-"""
     
 if __name__ == "__main__":
     main()
